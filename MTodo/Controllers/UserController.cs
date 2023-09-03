72071f54aeb334847f5311ca5b5e418fac4e2b01
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-09-03 21:38:27 +0630

    added profile

--- MTodo/Controllers/UserController.cs
+++ MTodo/Controllers/UserController.cs
@@ -1,14 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MTodo.Persistance.Context;
+using MTodo.Persistance.Tables;
 
 namespace MTodo.Controllers
 {
-	public class UserController:Controller
+	[Authorize]
+	public class UserController : Controller
 	{
 		private readonly MTodoContext context;
 		public UserController(MTodoContext context)
 		{
 			this.context = context;
+		}
+
+		[HttpGet]
+		public IActionResult profile()
+		{
+			string? claim = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (claim != null)
+			{
+				ProfileViewModel? model = context.users.Where(p => p.Id == Guid.Parse(claim)).Select(p => new ProfileViewModel
+				{
+					Name = p.UserName,
+					Email = p.Email,
+					Address = p.Address
+				}).FirstOrDefault();
+				return View("profile", model);
+			}
+			return RedirectToAction("Index","Home");
+		}
+
+		[HttpGet]
+		public IActionResult edit()
+		{
+			string? claim = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (claim != null)
+			{
+				ProfileViewModel? model = context.users.Where(p => p.Id == Guid.Parse(claim)).Select(p => new ProfileViewModel
+				{
+					Name = p.UserName,
+					Email = p.Email,
+					Address = p.Address
+				}).FirstOrDefault();
+				return View("edit", model);
+			}
+			return RedirectToAction("Index","Home");
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[ActionName("Edit")]
+		public IActionResult editAction(ProfileViewModel model)
+		{
+			string? claim = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (claim != null)
+			{
+				User? user = context.users.Where(p => p.Id == Guid.Parse(claim)).FirstOrDefault();
+				if(user !=null){
+					user.UserName=model.Name;
+					user.Email=model.Email;
+					user.Address=model.Address;
+					context.users.Update(user);
+					context.SaveChanges();
+					return RedirectToAction("profile","User");
+				}
+			}
+			return View("edit",model);
 		}
 	}
 }
