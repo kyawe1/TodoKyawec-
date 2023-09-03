98fc151b6cec767b23e3574ed4120596adf1f3fc
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-09-03 22:32:10 +0630

    todo user specific changes

--- MTodo/Controllers/TodoController.cs
+++ MTodo/Controllers/TodoController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MTodo.Persistance.Context;
 using MTodo.Persistance.Tables;
 
@@ -16,25 +17,40 @@ namespace MTodo.Controllers
 		}
 		public IActionResult index()
 		{
-			IEnumerable<TodoListViewModel> models=context.todos.Where(p=> p.isDeleted==false).Select(p=> new TodoListViewModel{
-				Id=p.Id.ToString(),
-				Task=p.Task,
-				isCompleted=p.IsCompleted
-			}).ToList();
-			return View("Index",models);
+			string? claim = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			IEnumerable<TodoListViewModel> models = Enumerable.Empty<TodoListViewModel>();
+			if (claim != null)
+			{
+				models = context.todos.AsNoTracking().Where(p => p.isDeleted == false && p.CreatedUser == Guid.Parse(claim)).Select(p => new TodoListViewModel
+				{
+					Id = p.Id.ToString(),
+					Task = p.Task,
+					isCompleted = p.IsCompleted
+				}).ToList();
+				return View("Index", models);
+			}
+			return View("Index", models);
 		}
 		[HttpGet]
 		public IActionResult detail(string Id)
 		{
-			TodoListViewModel? todo=context.todos.Where(q=> q.Id==Guid.Parse(Id)).Select(p=> new TodoListViewModel{
-				Task=p.Task,
-				isCompleted=p.isDeleted,
-				Id=p.Id.ToString()
-			}).FirstOrDefault();
-			if(todo==null){
-				return RedirectToAction("Index","Home");
+			string? claim = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			IEnumerable<TodoListViewModel> models = Enumerable.Empty<TodoListViewModel>();
+			TodoListViewModel? todo = null;
+			if (claim != null)
+			{
+				todo = context.todos.Where(q => q.Id == Guid.Parse(Id)).Select(p => new TodoListViewModel
+				{
+					Task = p.Task,
+					isCompleted = p.isDeleted,
+					Id = p.Id.ToString()
+				}).FirstOrDefault();
+				if (todo != null)
+				{
+					return View("Detail", todo);
+				}
 			}
-			return View("Detail",todo);
+			return RedirectToAction("Index", "Home");
 		}
 
 		[HttpGet]
@@ -45,14 +61,21 @@ namespace MTodo.Controllers
 		[HttpGet]
 		public IActionResult update(string Id)
 		{
-			TodoCreateViewModel? todo=context.todos.Where(q=> q.Id==Guid.Parse(Id)).Select(p=> new TodoCreateViewModel{
-				Task=p.Task,
-				isCompleted=p.isDeleted
-			}).FirstOrDefault();
-			if(todo==null){
-				return RedirectToAction("Index","Home");
+			string? claim = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			TodoCreateViewModel? todo = null;
+			if (claim != null)
+			{
+				todo = context.todos.Where(q => q.Id == Guid.Parse(Id) && Guid.Parse(claim) == q.CreatedUser).Select(p => new TodoCreateViewModel
+				{
+					Task = p.Task,
+					isCompleted = p.isDeleted
+				}).FirstOrDefault();
+				if (todo != null)
+				{
+					return View("Update", todo);
+				}
 			}
-			return View("Update",todo);
+			return RedirectToAction("Index", "Home");
 		}
 
 		[HttpPost]
@@ -61,38 +84,45 @@ namespace MTodo.Controllers
 		[Authorize]
 		public IActionResult insertAction(TodoCreateViewModel model)
 		{
-			if(ModelState.IsValid){
-				Claim Id=HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
-				Todo todo = new Todo(){
-					Task=model.Task,
-					IsCompleted=model.isCompleted,
-					CreatedUser=Guid.Parse(Id.Value)
+			if (ModelState.IsValid)
+			{
+				Claim Id = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+				Todo todo = new Todo()
+				{
+					Task = model.Task,
+					IsCompleted = model.isCompleted,
+					CreatedUser = Guid.Parse(Id.Value)
 				};
 				context.todos.Add(todo);
 				context.SaveChanges();
-				TempData["SuccessMessage"]="Todo is Not successfully Created!";
+				TempData["SuccessMessage"] = "Todo is Not successfully Created!";
 			}
-			TempData["ErrorMessage"]="Todo is successfully Created!";
+			TempData["ErrorMessage"] = "Todo is successfully Created!";
 			return RedirectToAction("Index", "Todo");
 		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		[ActionName("update")]
-		public IActionResult updateAction(TodoCreateViewModel model)
+		public IActionResult updateAction(string Id, TodoCreateViewModel model)
 		{
-			if(ModelState.IsValid){
-				Claim Id=HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
-				Todo todo = new Todo{
-					Task=model.Task,
-					IsCompleted=model.isCompleted,
-					CreatedUser=Guid.Parse(Id?.Value)
-				};
-				context.todos.Add(todo);
-				context.SaveChanges();
-				TempData["SuccessMessage"]="Todo is Not successfully Created!";
+			if (ModelState.IsValid)
+			{
+				Claim? claim = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+				if (claim != null)
+				{
+					Todo? todo = context.todos.Where(p => p.isDeleted == false && p.Id == Guid.Parse(Id) && p.CreatedUser == Guid.Parse(claim.Value)).FirstOrDefault();
+					if (todo != null)
+					{
+						todo.Task = model.Task;
+						todo.IsCompleted = model.isCompleted;
+						context.todos.Update(todo);
+						context.SaveChanges();
+						TempData["SuccessMessage"] = "Todo is Not successfully Updated!";
+					}
+				}
 			}
-			TempData["ErrorMessage"]="Todo is successfully Created!";
+			TempData["ErrorMessage"] = "Todo is successfully Created!";
 			return RedirectToAction("Index", "Todo");
 		}
 
@@ -104,11 +134,11 @@ namespace MTodo.Controllers
 		}
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public IActionResult finished(string Id,bool finished)
+		public IActionResult finished(string Id, bool finished)
 		{
-			if(!string.IsNullOrEmpty(Id)){
-				User? user=context.users.Find(Guid.Parse(Id));
-
+			if (!string.IsNullOrEmpty(Id))
+			{
+				User? user = context.users.Find(Guid.Parse(Id));
 			}
 			return Ok(new { message = "message created successfully." });
 		}
